f06478111b3fc725ffeaa3bbc55421e332b65311
21seya <wallacenevesk9@outlook.com>
2019-12-02 14:58:18 -0300

    extracao com sucesso

--- script.py
+++ script.py
@@ -3,9 +3,28 @@
 #r = requests.get(URL) 
 #print(r.text) 
 
+#from bs4 import BeautifulSoup
+#import re
+# import urllib2
+
+#url = "file:///C:/Users/wallace/Documents/teste_python/data/arquivo_01.html"
+#page = open(url)
+#soup = BeautifulSoup(page.read())
+
+#cities = soup.find_all('span', {'class' : 'city-sh'})
+
+#for city in cities:
+ #   print city
+
+#from bs4 import BeautifulSoup
+#import requests 
+#soup = BeautifulSoup(page.content, 'html.parser')
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 html = urlopen("http://127.0.0.1:8000/polls/")
 res = BeautifulSoup(html.read(),"html5lib");
-tags= res.find_all('table', class_='infraTable')
-print(tags)
+tags= res.find('fieldset', id='fldAssuntos')
+#tags = res.find('tr',class_='infraTrClara')
+#tags = res.find('form',attr={'id':'formProcessoLista'})
+print(tags.getText())
