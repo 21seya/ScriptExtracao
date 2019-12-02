4d1230c5b111e899f88131b0222da70de98d6c64
21seya <wallacenevesk9@outlook.com>
2019-12-02 12:50:28 -0300

    extracao com sucesso

--- script.py
+++ script.py
@@ -3,23 +3,6 @@
 #r = requests.get(URL) 
 #print(r.text) 
 
-#from bs4 import BeautifulSoup
-#import re
-# import urllib2
-
-#url = "file:///C:/Users/wallace/Documents/teste_python/data/arquivo_01.html"
-#page = open(url)
-#soup = BeautifulSoup(page.read())
-
-#cities = soup.find_all('span', {'class' : 'city-sh'})
-
-#for city in cities:
- #   print city
-
-#from bs4 import BeautifulSoup
-#import requests 
-#soup = BeautifulSoup(page.content, 'html.parser')
-
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 html = urlopen("http://127.0.0.1:8000/polls/")
